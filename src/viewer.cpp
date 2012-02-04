43d25c1baa0b9a65260a9018583db18d832cbcc4
Pascal Spörri <pascal@moeeeep.com>
2012-02-05 00:42:55 +0100

    GPLed

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,3 +1,19 @@
+// This file belongs to Donkey²: Yet another XYZ OpenGL Dataviewer
+//     https://github.com/moeeeep/donkey2
+// Copyright (C) 2011  Pascal Spörri <pascal@moeeeep.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "viewer.h"
 
 using namespace std;
@@ -18,7 +34,6 @@ void Viewer::init() {
     glEnable(GL_TEXTURE_2D);
 
     QImage b("particle.bmp");
-//    QImage b("image.jpeg");
 
     QImage t;
     t = QGLWidget::convertToGLFormat(b);
@@ -85,9 +100,6 @@ void Viewer::animate() {
 
 QString Viewer::helpString() const
 {
-  QString text("<h2>F a s t D r a w</h2>");
-  text += "The <code>fastDraw()</code> function is called instead of <code>draw()</code> when the camera ";
-  text += "is manipulated. Providing such a simplified version of <code>draw()</code> allows for interactive ";
-  text += "frame rates when the camera is moved, even for very complex scenes.";
+  QString text("<h2>Donkey2</h2>");
   return text;
 }
